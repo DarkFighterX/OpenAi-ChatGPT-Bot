fca4e68ffce0cabbfb61a567452b1812aeece9a6
DARK FIGHTER X <122691971+DarkFighterX@users.noreply.github.com>
2023-06-27 21:39:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const chalk = require('chalk')
 /*
 🤖 𝗕𝗢𝗧 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OFFLINE = false // if you want put bot offline put true
+global.OFFLINE = true // if you want put bot offline put true
 global.BOT_PRIVET = false // if you want use bot privet 
 global.AUTO_CHATGPT = false // if you want auto ai bot mod
 global.READ_MESSAGE = false // if you want read message from bot
@@ -16,9 +16,9 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
-global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers
+global.OWNER_NAME = 'Sashika' // Owner Name
+global.OWNER_NUMBER = ['94757258337'] //Owner Number
+global.BOT_ADMINS = ['94752836001','94717671650'] //Bot admins numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 
 
@@ -27,7 +27,7 @@ global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and 
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-global.AI_PASSWORD = ''
+global.AI_PASSWORD = 'king-nima-gpt-bot'
 global.SESSION_ID = ''
 
 /*
